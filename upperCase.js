8e60221797ac66ba58fbeab46cd1fdf31c5176cf
Salvador Martinez Espinosa <cchhava@gmail.com>
2022-01-13 17:25:01 -0600

    Actualizacion

---
+++ upperCase.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    var uc = require('C:/Users/cchha/node_modules/upper-case');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(uc.upperCase("Hello World, I'm here!"));
+  res.end();
+}).listen(8080);
